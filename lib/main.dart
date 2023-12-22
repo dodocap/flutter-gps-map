5c2e7004e8dfe4c1c03b229a36cca157822e1307
dodocap <dodocap@naver.com>
2023-12-22 22:05:35 +0900

    feat: Google Map 패키지 추가
    1. AOS / iOS 설정
    2. 샘플 코드 삽입

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:orm_gps_map/gps_map_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
+      home: GPSMapScreen(),
     );
   }
 }
